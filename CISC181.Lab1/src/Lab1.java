a83ef3e2d1e9fe39b1c7302a69ded0bc9f0b8cb9
Manuel <Manuel@128.4.194.177>
2015-09-10 23:16:05 -0400

    Draft asking all of the required information without equations.

--- CISC181.Lab1/src/Lab1.java
+++ CISC181.Lab1/src/Lab1.java
@@ -1,6 +1,9 @@
+// Import the Scanner utility in order to ask the user for input 
 import java.util.Scanner;
+// Call the Apache Finance Class to run Calculations
 
-//Manuel Cuesta and Russ Stump, CISC181 Lec 011
+
+//Manuel Cuesta and Russ Stump, CISC 181 Lec 011
 
 public class Lab1 {
 
@@ -16,26 +19,36 @@ public class Lab1 {
 		float avg_return = input.nextFloat();
 		
 		System.out.print("How long do you plan to draw your ");
-		System.out.println(" savings after retirements? ");
+		System.out.println(" savings after retirement? ");
 		int years_draw = input.nextInt();
 		
-		System.out.print("What is your expected annual return ");
+		System.out.print("What would your expected annual return ");
 		System.out.println(" after you retire?: ");
 		float return_after_retire = input.nextFloat();
 		
-		System.out.print("What is your required monthly income ");
+		System.out.print("What would your required monthly income ");
 		System.out.println(" after you retire? ");
+		float req_income = input.nextFloat();
+		
+		System.out.print("What would be your monthly SSI?: ");
 		float ssi = input.nextFloat();
 		
 		//Just to make sure the variables are being assigned
 		//and they all worked
-		/*
+		
+		System.out.print("\n");
 		System.out.print(years);
+		System.out.print("\n");
 		System.out.print(avg_return);
+		System.out.print("\n");
 		System.out.print(years_draw);
+		System.out.print("\n");
 		System.out.print(return_after_retire);
+		System.out.print("\n");
+		System.out.print(req_income);
+		System.out.print("\n");
 		System.out.print(ssi);
-		*/
+		
 	}
 
 }
